c04c6a67b176bbd06e3cd6ca9560ef652db05d7d
AIsamet <isamet2067@gmail.com>
2024-05-08 16:21:59 +0200

    added profile page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RecapitulatifComponent } from './recapitulatif/recapitulatif.component'
 import { ProductCatalogComponent } from './product-catalog/product-catalog.component';
 import { CardComponent } from './card-module/card/card.component';
 import { CartMenuComponent } from './cart-menu/cart-menu.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 
 export const routes: Routes = [
@@ -14,5 +15,6 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'products', component: ProductCatalogComponent},
     { path: 'cards', component: CardComponent},
-    { path: 'cart', component: CartMenuComponent}
+    { path: 'cart', component: CartMenuComponent},
+    { path: 'profile', component: ProfilePageComponent}
 ];
